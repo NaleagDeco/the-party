b3ffe1c17ae79df95a0aeb9abf41668194f71fa5
Gaelan D'costa <gdcosta@gmail.com>
2014-12-17 20:48:25 -0500

    We can render stuff to screen!

--- src/the_party/core.clj
+++ src/the_party/core.clj
@@ -11,11 +11,25 @@
 ;;;; UPDATE
 
 ;;;; VIEW
+(defn render-tile [tile]
+  (case tile
+    :vertical-wall \|
+    :horizontal-wall \-
+    :open-door \+
+    :passage \#
+    :empty-space \.
+    :inaccessible \space))
+
+(defn render [term state]
+  (doseq [tile (seq (state :terrain))]
+    (t/put-character term (render-tile (second tile))
+                     (second (first tile)) (first (first tile)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [state (gs/create-game "Hello")
         term (t/get-terminal)]
     (t/in-terminal term
-                      (t/put-string term "Hello!")
-                      (t/get-key-blocking term))))
+                   (render term state)
+                   (t/get-key-blocking term))))
