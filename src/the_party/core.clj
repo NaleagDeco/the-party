43dce50f29be40e29ddc8da109eb051cc0715c7d
Gaelan D'costa <gdcosta@gmail.com>
2014-12-18 16:24:37 -0500

    We can move around and quit

--- src/the_party/core.clj
+++ src/the_party/core.clj
@@ -2,13 +2,27 @@
   (:require [the-party.game-state :as gs])
   (:require [lanterna.screen :as s])
   (:require [lanterna.terminal :as t])
+  (:require [reagi.core :as r])
   (:gen-class))
 
 ;;;; INPUTS
+(def input (r/events))
+
+(defn process-input! [char]
+  (do
+    (case char
+      \h (r/deliver input :player-left)
+      \j (r/deliver input :player-down)
+      \k (r/deliver input :player-up)
+      \l (r/deliver input :player-right)
+      nil)
+    char))
 
 ;;;; MODEL
 
 ;;;; UPDATE
+(def game-state
+  (r/reduce gs/process-input (gs/create "Hello") input))
 
 ;;;; VIEW
 (defn render-tile [tile]
@@ -20,16 +34,25 @@
     :empty-space \.
     :inaccessible \space))
 
+(defn render-player [term state]
+  (let [coords (state :player-coords)]
+    ; lanterna uses x y coords but we use row column
+    (t/put-character term \@ (second coords) (first coords))))
+
 (defn render [term state]
+  (t/clear term)
   (doseq [tile (seq (state :terrain))]
     (t/put-character term (render-tile (second tile))
-                     (second (first tile)) (first (first tile)))))
+                     (second (first tile)) (first (first tile))))
+  (render-player term state))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [state (gs/create-game "Hello")
-        term (t/get-terminal)]
+  (let [term (t/get-terminal)]
     (t/in-terminal term
-                   (render term state)
-                   (t/get-key-blocking term))))
+                   (loop [key nil]
+                     (if (= key \q) "Goodbye!"
+                         (do
+                           (render term @game-state)
+                           (recur (process-input! (t/get-key-blocking term)))))))))
--- src/the_party/game_state.clj
+++ src/the_party/game_state.clj
@@ -2,8 +2,20 @@
   (:require [clojure.java.io :as io])
   (:require [the-party.builders :as builder]))
 
-(defn create-game [player]
+(defn create [player]
   { :player player
    :terrain (builder/file->terrain (io/resource "map.txt"))
    :player-coords [0 0]
    :status "Welcome to The Party!" })
+
+(defn move-player [state offset]
+  (let [old-coords (state :player-coords)
+        new-coords (map #(reduce + %) (map vector old-coords offset))]
+    (assoc state :player-coords new-coords)))
+
+(defn process-input [state input]
+  (case input
+    :player-left (move-player state [0 -1])
+    :player-right (move-player state [0 1])
+    :player-up (move-player state [-1 0])
+    :player-down (move-player state [1 0])))
