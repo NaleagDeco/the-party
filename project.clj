981ce046b7ddb36efb098ec1470f0bf4600212f4
Gaelan D'costa <gdcosta@gmail.com>
2014-12-17 20:07:14 -0500

    We can display a screen.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clojure-lanterna "0.9.4"]
-                 [jamesmacaulay/zelkova "0.2.0"]]
+                 [reagi "0.10.1"]]
   :main ^:skip-aot the-party.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/the_party/core.clj
+++ src/the_party/core.clj
@@ -1,7 +1,21 @@
 (ns the-party.core
+  (:require [the-party.game-state :as gs])
+  (:require [lanterna.screen :as s])
+  (:require [lanterna.terminal :as t])
   (:gen-class))
 
+;;;; INPUTS
+
+;;;; MODEL
+
+;;;; UPDATE
+
+;;;; VIEW
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [state (gs/create-game "Hello")
+        term (t/get-terminal)]
+    (t/in-terminal term
+                      (t/put-string term "Hello!")
+                      (t/get-key-blocking term))))
