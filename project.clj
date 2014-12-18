4a16c636aab2228cb56deb113f94982291259aa6
Gaelan D'costa <gdcosta@gmail.com>
2014-12-18 16:26:38 -0500

    Fixed some lein warnings.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clojure-lanterna "0.9.4"]
-                 [reagi "0.10.1"]]
+                 [reagi "0.10.1" :exclusions [org.clojure/clojure]]]
   :main ^:skip-aot the-party.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
