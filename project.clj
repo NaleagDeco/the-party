5b1d1985a40b9a9f2c987820d48b56ceea830fd3
Gaelan D'costa <gdcosta@gmail.com>
2017-04-19 20:49:29 -0400

    Update dependency versions

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [clojure-lanterna "0.9.4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.442"]
+                 [clojure-lanterna "0.9.7"]
                  [reagi "0.10.1" :exclusions [org.clojure/clojure]]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot the-party.core
